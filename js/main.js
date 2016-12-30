41c42de8bc76baf3ecce86bc0f03f58752dedfa6
Aleksey Bestuzhev <aleksey.bestuzhev@gmail.com>
2016-12-30 17:11:07 +0300

    changes

--- js/main.js
+++ js/main.js
@@ -35,7 +35,34 @@ $(document).ready(function() {
         adaptiveHeight: false,
         slidesToShow: 4,
         slidesToScroll: 1,
-        variableWidth: true
+        variableWidth: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
     });
 
     $('.stock-slider').slick({
@@ -43,6 +70,18 @@ $(document).ready(function() {
         autoplaySpeed: 5000,
         dots: false,
         arrows: true,
+        infinite: true,
+        speed: 500,
+        adaptiveHeight: false,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+
+    $('.home-slider').slick({
+        autoplay: true,
+        autoplaySpeed: 5000,
+        dots: true,
+        arrows: false,
         infinite: true,
         speed: 500,
         adaptiveHeight: false,
