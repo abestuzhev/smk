b75b6ef3557f4862aa416018b7618da486e45209
Aleksey <aleksey.bestuzhev@gmail.com>
2016-12-30 07:27:02 +0300

    changes

---
+++ js/main.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {
+
+    /*tabs*/
+    $(".tabs-menu a").click(function(event) {
+        event.preventDefault();
+        $(this).parent().addClass("current");
+        $(this).parent().siblings().removeClass("current");
+        var tab = $(this).attr("href");
+        $(".tab-content").not(tab).css("display", "none");
+        $(tab).fadeIn();
+    });
+
+    /*slider*/
+    $('.objects-slider').slick({
+        autoplay: false,
+        autoplaySpeed: 5000,
+        dots: false,
+        arrows: true,
+        infinite: true,
+        speed: 500,
+        adaptiveHeight: false,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        variableWidth: true
+    });
+
+
+    $('.realty-slider').slick({
+        autoplay: false,
+        autoplaySpeed: 5000,
+        dots: false,
+        arrows: true,
+        infinite: true,
+        speed: 500,
+        adaptiveHeight: false,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        variableWidth: true
+    });
+
+    $('.stock-slider').slick({
+        autoplay: false,
+        autoplaySpeed: 5000,
+        dots: false,
+        arrows: true,
+        infinite: true,
+        speed: 500,
+        adaptiveHeight: false,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+
+});
