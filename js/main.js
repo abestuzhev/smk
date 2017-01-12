91da864996e827d99c1ea4701245242e8751ceda
Aleksey Bestuzhev <aleksey.bestuzhev@gmail.com>
2017-01-12 15:30:21 +0300

    changes

--- js/main.js
+++ js/main.js
@@ -227,6 +227,30 @@ $(document).ready(function() {
 
     $('.phone-mask').mask('+7(000)000-00-00');
     $('.mail-mask').mask('+7(000)000-00-00');
+
+
+    //кнопка вверх
+    var offset = 300,
+        offset_opacity = 1200,
+        scroll_top_duration = 700,
+        $back_to_top = $('.cd-top');
+    //hide or show the "back to top" link
+    $(window).scroll(function(){
+        ( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
+        if( $(this).scrollTop() > offset_opacity ) {
+            $back_to_top.addClass('cd-fade-out');
+        }
+    });
+    //smooth scroll to top
+    $back_to_top.on('click', function(event){
+        event.preventDefault();
+        $('body,html').animate({
+                scrollTop: 0 ,
+            }, scroll_top_duration
+        );
+    });
+
+
 });
 
 lightGallery(document.getElementById('anchor-tag'));
