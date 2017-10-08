a91b844a9e397bc5d405cc9f0dff18924e55a1dd
Aleksey Bestuzhev <aleksey.bestuzhev@gmail.com>
2017-10-08 14:04:42 +0300

    changes

--- js/manual.js
+++ js/manual.js
@@ -1,31 +1,32 @@
 $(function(){
+    /*переменные*/
+    var $linkDoc = $('.manual-sub-list__link');//ссылка на конечный документ
+    var $titleSection = $('.manual-sub-list__title');//заголовок раздела
+
     /*скрыть верхнее пояснение о разделе*/
     $('.manual-info__close').on('click', function(){
         $(this).parents('.manual-info').hide(200);
     });
 
     /*показ подпунктов*/
-    $('.manual-sub-list__title').on('click', function(){
-        $(this).toggleClass('sub-list__open');
-        $(this).siblings('.manual-sub-list').toggle(150);
+    $titleSection.on('click', function(e){
+        if($(this).parents('.manual-list__item').children().hasClass('manual-sub-list')){
+            $(this).toggleClass('sub-list__open');
+            $(this).siblings('.manual-sub-list').toggle(150);
+            console.log('условие выполняется');
+            e.preventDefault();
+        }
     });
-    $('.manual-sub-list__title').parent('.manual-list__item').has('ol').bind('click',function(){
-        $(this).toggleClass('sub-list__open');
-        $(this).siblings('.manual-sub-list').toggle(150);
-        return false;
-    });
-
 
     /*hover для нумерации*/
-    $('.manual-sub-list__title').hover(function(){
+    $titleSection.hover(function(){
         $(this).parent('.manual-list__item').toggleClass('manual-list__hover');
     });
-    $('.manual-sub-list__link').hover(function(){
+    $linkDoc.hover(function(){
         $(this).parent('.manual-list__item').toggleClass('manual-list__hover');
     });
 
     /*добавление иконки-стрелочки только элементам, которые имеют подразделы*/
     $('.manual-list__item').has('ol').children('.manual-sub-list__title').addClass("manual-sub-list__title--icon");
-
 
 });
