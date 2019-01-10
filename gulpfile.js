4ff5dc00857e6b9a946ca6a3489253199cd19afc
Aleksey Bestuzhev <aleksey.bestuzhev@gmail.com>
2019-01-10 23:34:41 +0300

    changes

--- gulpfile.js
+++ gulpfile.js
@@ -9,54 +9,54 @@ var gulp = require('gulp'),
     browserSync = require('browser-sync').create();
 
 
-var srcDir = 'src/';
+// var srcDir = 'src/';
 
 gulp.task('sass', function(){
-  return gulp.src(srcDir + 'scss/**/*.scss')
+    return gulp.src('src/scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(prefix('last 3 version'))
-    .pipe(gulp.dest('css/'))
-    .pipe(browserSync.stream())
+        .pipe(prefix('last 3 version'))
+        .pipe(gulp.dest('css/'))
+        .pipe(browserSync.stream())
 });
 
 gulp.task('js', function(){
-  return gulp.src('js/**/*.js')
-    .pipe(gulp.dest('js/'))
-    .pipe(browserSync.stream())
+    return gulp.src('js/**/*.js')
+        .pipe(gulp.dest('js/'))
+        .pipe(browserSync.stream())
 });
 
 gulp.task('html', function(){
-  return gulp.src('**/*.html')
-    .pipe(browserSync.stream())
+    return gulp.src('**/*.html')
+        .pipe(browserSync.stream())
 });
 
 gulp.task('css', function(){
-  return gulp.src('css/**/*.css')
-    .pipe(browserSync.stream())
+    return gulp.src('css/**/*.css')
+        .pipe(browserSync.stream())
 });
 
 
 gulp.task('browser-sync', function(){
-  browserSync.init({
-    port: 8080,
-    server: {
-      baseDir: './'
-    }
-  })
+    browserSync.init({
+        port: 8080,
+        server: {
+            baseDir: './'
+        }
+    })
 });
 
 gulp.task('connect', function() {
-  connect.server({
-    root: '',
-    livereload: true
-  });
+    connect.server({
+        root: '',
+        livereload: true
+    });
 });
 
 gulp.task('watch', function(){
-  gulp.watch(srcDir + 'scss/**/*.scss', ['sass'])
-  gulp.watch(srcDir + 'js/**/*.js', ['js'])
-  gulp.watch('**/*.html', ['html'])
-  gulp.watch('css/**/*.css', ['css'])
+    gulp.watch('src/scss/**/*.scss', ['sass']);
+    gulp.watch('src/js/**/*.js', ['js']);
+    gulp.watch('**/*.html', ['html']);
+    gulp.watch('css/**/*.css', ['css'])
 });
 
-gulp.task('default', ['html', 'css', 'sass', 'js', 'browser-sync', 'watch'])
+gulp.task('default', ['html', 'css', 'sass', 'js', 'browser-sync', 'watch']);
